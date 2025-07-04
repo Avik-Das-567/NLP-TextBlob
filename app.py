2207a7f961e08e68ad1b529f2e99004b928ab501
Avik Das <avikdas567@gmail.com>
2025-07-04 15:05:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from textblob import TextBlob
+
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('averaged_perceptron_tagger_eng')
+
+# Sentiment Analysis
+text = "I love learning Python!"
+blob = TextBlob(text)
+st.header("Sentiment Analysis")
+st.write("Input Text :-", text)
+st.write("Output :-", blob.sentiment)
+
+# Tokenization
+blob = TextBlob("Natural Language Processing is fun.")
+st.header("Tokenization")
+st.write("Input Text :- Natural Language Processing is fun.")
+st.write("Words :-", blob.words)
+st.write("Sentences :-", blob.sentences)
+
+# POS Tagging
+blob = TextBlob("Python is a powerful language")
+st.header("POS Tagging")
+st.write("Input Text :- Python is a powerful language")
+st.write("Output :-", blob.tags)
